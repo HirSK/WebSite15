c1e7f631248e596469eb68d259cd23f36d3653fa
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-07-24 23:07:56 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 7/24/2017
+ * Time: 5:17 PM
+ */
+?>
+
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
+
