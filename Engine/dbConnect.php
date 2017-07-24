60f250f5bf472e40a2e16b0f3f10624d8414e90a
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-07-24 23:09:23 +0530

    Add files via upload

---
+++ Engine/dbConnect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 7/24/2017
+ * Time: 9:38 PM
+ */
+$conn = mysqli_connect("localhost","root","","project15db");
+
+if(!$conn){
+    die("Connection failed : ".mysqli_connect_error());
+}
+
+?>
